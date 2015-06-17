3d691c98e8d635527eafecfa91cf2763f08f8d75
arendtrasmussen <arendtrasmussen@gmail.com>
2015-06-17 12:14:36 +0200

    sadas

--- app/src/main/java/dk/lycksgalleri/countrynamepls/MainActivity.java
+++ app/src/main/java/dk/lycksgalleri/countrynamepls/MainActivity.java
@@ -1,19 +1,34 @@
 package dk.lycksgalleri.countrynamepls;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TableLayout;
 
 
-public class MainActivity extends ActionBarActivity {
-
+public class MainActivity extends Activity {
+    TableLayout buttonOptions;
+    ImageView flagView;
+    int optionButtons;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        optionButtons = 2;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buttonOptions =
+                (TableLayout) findViewById(R.id.buttonTableLayout);
+
+        flagView = (ImageView) findViewById(R.id.flagView);
+
+
+
     }
 
+    public void createOptionButtons(){
 
+    }
 
 }
